53cf7794900cebea2de1ea57f10381d4933c5b12
Hao Jiang <joshtrick@outlook.com>
2018-07-22 01:11:04 +0800

    Hello Corerain

--- inc/utility/corerain_printer.h
+++ inc/utility/corerain_printer.h
@@ -1,7 +1,18 @@
+#ifndef CORERAIN_PRINTER_H
+#define CORERAIN_PRINTER_H
+
 #include <stdio.h>
 #include <time.h>
+
+#define DEBUG_TEST 1
+#define print_debug(fmt, ...) \
+  do{ \
+    if(DEBUG_TEST) printf("[CORERAIN] File %s, line %d, %s():\n[CORERAIN] " fmt, __FILE__, \
+                          __LINE__, __func__, __VA_ARGS__); \
+  }while(0)
 
 void print_corerain();
 void print_CRT();
 void print_program_info(const char *prog_name);
-void printf_cr(int line, const char *func, const char *file, const char *format, ...);
+
+#endif //CORERAIN_PRINTER_H
--- test/logo.cpp
+++ test/logo.cpp
@@ -1,9 +1,8 @@
 #include "utility/corerain_printer.h"
-
 int main()
 {
   print_CRT();
   print_program_info(__FILE__);
-  printf_cr(__LINE__, __func__, __FILE__, "Hello");
+  print_debug("Something wrong: %s\n", "Hello");
   return 0;
 }
